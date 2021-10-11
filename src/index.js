4d01ab0996cd7b6896e25b432beca6ff59055762
Roman Grigoriadi <roman.grigoriadi@dhl.com>
2021-10-11 09:32:28 +0200

    Add backup HTML of grigoriadi.info site

--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const move = {
+    X:{literal:"X"},
+    Y:{literal:"Y"}
+}
+move.X.next = move.Y;
+move.Y.next = move.X;
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+}
 function Square(props) {
     return (
         <button className="square" onClick={() => props.onclick(props.buttonId)}>
@@ -15,21 +34,23 @@ class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            nextMove: move.X
         };
     }
     handleClick = (id) => {
         const sqaresCopy = this.state.squares.slice();
-        sqaresCopy[id] = "X";
-        this.setState({squares: sqaresCopy})
-        console.log("State of ID " + id + " changed to X");
+        sqaresCopy[id] = this.state.nextMove.literal;
+
+        this.setState({squares: sqaresCopy, nextMove: this.state.nextMove.next})
+        console.log("State of ID " + id + " changed to " + sqaresCopy[id]);
     }
     renderSquare(i) {
         return <Square value={this.state.squares[i]} buttonId={i} onclick={(buttonId) => this.handleClick(buttonId)} />;
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + this.state.nextMove.literal;
 
         return (
             <div>
