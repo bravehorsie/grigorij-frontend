a7194a96d5c65995feb4d766262a05abf7e4654f
Roman Grigoriadi <roman.grigoriadi@gmail.com>
2021-11-28 11:12:32 +0100

    Update image loading, add menu.

--- src/index.js
+++ src/index.js
@@ -13,19 +13,24 @@ class Mynavbar extends React.Component {
     render() {
         return (
             <Navbar bg="light" expand="lg" className="fixed-top">
-                <Container>
+                <Container className={"flex-row-reverse"}>
                     <Navbar.Brand href="#home">Григорий Фемистоклович Григориади</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Воспоминания</Nav.Link>
-                            <Nav.Link href="#link">Стихи</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            <NavDropdown title="Воспоминания" id="navbar-memories">
+                                <NavDropdown.Item href="#1">Человек Родился</NavDropdown.Item>
+                                <NavDropdown.Item href="#2">Война</NavDropdown.Item>
+                                <NavDropdown.Item href="#3">Агитбригады</NavDropdown.Item>
+                                <NavDropdown.Item href="#4">Знакомство</NavDropdown.Item>
+                                <NavDropdown.Item href="#5">Мой избранный народ</NavDropdown.Item>
+                                <NavDropdown.Item href="#6">Пенаты</NavDropdown.Item>
+                                <NavDropdown.Item href="#7">Поэзия, творчество</NavDropdown.Item>
+                                <NavDropdown.Item href="#8">Характер</NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Стихи" id="navbar-poems">
+                                <NavDropdown.Item href="#">Звездопады</NavDropdown.Item>
+                                <NavDropdown.Item href="#">Моей бессоницы друзья</NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
                     </Navbar.Collapse>
@@ -35,21 +40,37 @@ class Mynavbar extends React.Component {
     }
 }
 
+class Image extends React.Component {
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {loaded: false}
+    }
+
+    render() {
+        const img = this.props.img;
+        return <div className="d-flex justify-content-center">
+            <div className={"imageHolder"}>
+                <img src={img.name} alt={img.description}
+                     onLoad={() => {this.setState({loaded: true})}}
+                     className="chapterImage"/>
+                <div className={this.state.loaded ? "image-label visible" : "image-label hidden"}>{img.description}</div>
+            </div>
+        </div>
+    }
+}
+
 class Section extends React.Component {
     render() {
         return (
             <div>
-                <h2>{this.props.heading}</h2>
+                <h2 className={"section-heading"}>{this.props.heading}</h2>
                 {this.props.paragraphs.map(function (paragraph, i) {
 
-                    return <div className={paragraph.image == null || i % 2 === 0 ? "d-flex flex-row flex-wrap" : "d-flex flex-row-reverse flex-wrap"}>
+                    return <div className={"sectionHolder"}>
                         {
-                            paragraph.image ?
-                                <div className="imageHolder">
-                                    <img src={paragraph.image.name} alt={paragraph.image.description}
-                                         className="chapterImage"/>
-                                    <div className={"image-label"}>{paragraph.image.description}</div>
-                                </div>
+                            paragraph.image != null ?
+                                <Image img={paragraph.image} />
                                 :
                                 null
                         }
@@ -74,7 +95,8 @@ class Chapter extends React.Component {
 
     render() {
         return <div className={"chapter"}>
-            <h1>{this.state.name}</h1>
+            <a className={"anchorLink"}  id={this.props.sectionId}>anchor link</a>
+            <h1 className={"chapter-heading"}>{this.state.name}</h1>
             {
                 this.state.sections.map(function (section, i) {
                     return <Section heading={section.heading} paragraphs={section.paragraphs} key={i} />
